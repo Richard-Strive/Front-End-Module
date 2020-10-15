dd458b87059a553e355c323227cc717841a7ae05
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2020-10-15 16:46:41 +0200

    Add files via upload

---
+++ M2-D4/praxi.js
@@ -0,0 +1,17 @@
+const numberTeam= document.querySelector("input.number-team")
+
+const form= document.querySelector("input.form-control");
+
+const uL= document.createElement("ul");
+
+const li= document.createElement("il")
+
+const assignBtn= function(click){
+
+li.innerHTML= "form.value"
+
+ul.appendChild(li)
+
+document.body.appendChild(uL)
+
+}
